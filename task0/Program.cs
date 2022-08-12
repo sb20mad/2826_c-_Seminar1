b7325a85fa0cd7f5aba6b0052a253f2590c9bbf8
sb20mad <sb20mad@gmail.com>
2022-08-13 00:09:55 +0300

    add task2

---
+++ task0/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("ВВедите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number*number);
+
+
+
---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("ВВедите число 1 ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ВВедите число 2 ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+// if 
+int result = numberA / numberB;
+//Console.WriteLine(result);
+if (result == numberB)
+Console.WriteLine("ДА");
+else
+{
+    Console.WriteLine("НЕТ");
+}
